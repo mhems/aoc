69d402b04d44a6570976297296ee2058d3feb70e
matthew <matthems@gmail.com>
2023-12-24 13:45:11 -0500

    solved with a lot of help from reddit

--- 2023/day5/a.py
+++ 2023/day5/a.py
@@ -1,11 +1,9 @@
 from sys import argv
 from collections import namedtuple as nt
-from itertools import pairwise
-from functools import cache
 
 MapEntry = nt('MapEntry', ['dst', 'src', 'len'])
+Range = nt('Range', ['start', 'stop'])
 
-@cache
 def map_get(map: (MapEntry), value: int):
     for entry in map:
         if entry.src <= value <= entry.src + entry.len:
@@ -32,15 +30,11 @@ for line in lines:
                 count += 1
                 maps[count] = []
                 continue
-            maps[count] = tuple(maps[count])
             count += 1
             maps[count] = []
 maps.pop(0)
 maps.pop(0)
-maps[-1] = tuple(maps[-1])
-maps = tuple(maps)
 
-@cache
 def get_location_from_seed(seed: int) -> int:
     value = seed
     for m in maps:
@@ -50,11 +44,35 @@ def get_location_from_seed(seed: int) -> int:
 answer = min(get_location_from_seed(i) for i in seeds)
 print(answer)
 
-seed_ranges = [(seeds[i],seeds[i+1]) for i in range(0, len(seeds), 2)]
+seed_ranges = [Range(seeds[i],seeds[i]+seeds[i+1]) for i in range(0, len(seeds), 2)]
 
-def get_location_from_seed_range(start, length):
-    print(start, length)
-    return min(get_location_from_seed(i) for i in range(start, start + length))
+# adapted from https://www.reddit.com/r/adventofcode/comments/18b4b0r/2023_day_5_solutions/
+def translate_ranges(entries: [MapEntry], ranges: [Range]) -> [Range]:
+    translated_ranges = []
+    for entry in entries:
+        not_translated_ranges = []
+        while ranges:
+                cur = ranges.pop()
+                before = Range(cur.start, min(cur.stop, entry.src))
+                if before[1] > before[0]:
+                    not_translated_ranges.append(before)
 
-answer = min(get_location_from_seed_range(start, length) for (start, length) in seed_ranges)
-print(answer)
+                overlap = Range(max(cur.start, entry.src), min(cur.stop, entry.src + entry.len))
+                if overlap[1] > overlap[0]:
+                    # only have to translate the range that overlaps with map entry
+                    delta = entry.dst - entry.src
+                    translated_ranges.append(Range(overlap[0] + delta, overlap[1] + delta))
+
+                after = Range(max(cur.start, entry.src + entry.len), cur.stop)
+                if after[1] > after[0]:
+                    not_translated_ranges.append(after)
+        ranges = not_translated_ranges
+    return translated_ranges + ranges
+
+starts = []
+for start, stop in seed_ranges:
+    ranges = [Range(start, stop)]
+    for m in maps:
+        ranges = translate_ranges(m, ranges)
+    starts.append(min(r.start for r in ranges))
+print(min(starts))
