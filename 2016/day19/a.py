5be0604f0477a922d7fc5d60996c1d69a9ebfbf9
matthew <matthems@gmail.com>
2024-02-10 12:53:55 -0500

    part 2

--- 2016/day19/a.py
+++ 2016/day19/a.py
@@ -1,5 +1,6 @@
 from sys import argv
 from math import log
+from collections import deque
 
 n = int(argv[1])
 
@@ -8,22 +9,21 @@ def elf(n: int) -> int:
 
 print(elf(n))
 
-def cycle(n: int) -> [int]:
-    elves = list(range(1, n + 1))
-    i = 0
-    n = len(elves)
-    while n > 1:
+def cycle_deque(n: int) -> int:
+    elves = deque(range(1, n + 1))
+    for n in (range(len(elves), 1, -1)):
         delta = n // 2
-        #print(i, elves[i], delta, len(elves), elves)
-        elves.pop((i + delta) % n)
-        i += 1
-        n -= 1
-        if i > n:
-            i = 0
-        if n % 100000 == 0:
-            print('.')
+        elves.rotate(-delta)
+        elves.popleft()
+        elves.rotate(delta - 1)
     return elves[0]
 
-print(cycle(n))
+def compute(n: int) -> int:
+    cube = 3 ** int(log(n, 3))
+    if cube == n:
+        return n
+    if n <= 2 * cube:
+        return n - cube
+    return cube + 2 * (n - 2 * cube)
 
-# print([cycle(i) for i in range(2, 10)])
+print(compute(n))
