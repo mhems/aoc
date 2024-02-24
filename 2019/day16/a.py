81bc57fc7b5130d9851e19d6114560dd22d17154
matthew <matthems@gmail.com>
2024-02-24 17:49:45 -0500

    even with numpy, brute force wont do

--- 2019/day16/a.py
+++ 2019/day16/a.py
@@ -1,24 +1,51 @@
 from sys import argv
-from itertools import cycle
+import numpy as np
+from math import ceil
 
-def make_pattern(i: int) -> [int]:
-    return [0] * i + [1] * i + [0] * i + [-1] * i
+def make_pattern(i: int, n: int) -> [int]:
+    pattern = [0] * i
+    count = i
+    if count < n + 1:
+        pattern += [1] * i
+        count += i
+    if count < n + 1:
+        pattern += [0] * i
+        count += i
+    if count < n + 1:
+        pattern += [-1] * i
+        count += i
+    if count < n + 1:
+        rep = ceil(n/len(pattern)) + 1
+        pattern *= rep
+    pattern.pop(0)
+    if len(pattern) > n:
+        pattern = pattern[:n]
+    return pattern
 
-def fft(number: [int]) -> [int]:
-    answer = [None] * len(number)
-    number.insert(0, 0)
-    for i in range(1, len(number)):
-        pattern = make_pattern(i)
-        n = sum(a * b for a, b in zip(cycle(pattern), number))
-        answer[i-1] = abs(n) % 10
-    return answer
+def make_patterns(n: int):
+    return np.array([make_pattern(i + 1, n) for i in range(n)])
 
-def phased_fft(number: [int], n: int = 100) -> str:
-    for _ in range(n):
-        number = fft(number)
-    return ''.join(map(str, number))[:8]
+matmod = np.vectorize(lambda n: abs(n) % 10)
 
-numbers = [[int(digit) for digit in str(int(line.strip()))] for line in open(argv[1]).readlines()]
+def fft(number: [int], pattern) -> [int]:
+    product = np.matmul(pattern, number)
+    return matmod(product)
 
+def phased_fft(number, phases: int = 100) -> str:
+    pattern = make_patterns(len(number))
+    number = [int(digit) for digit in number]
+    number = np.array(number).reshape((len(number), 1))
+    for _ in range(phases):
+        number = fft(number, pattern)
+    return ''.join(map(str, number.flat))
+
+def part2(number: str, repeat=10_000) -> str:
+    index = int(number[:7])
+    result = phased_fft(number * repeat)
+    return ''.join(str(result[i][0]) for i in range(index, index+8))
+
+numbers = [line.strip() for line in open(argv[1]).readlines()]
 for number in numbers:
-    print(phased_fft(number))
+    print(phased_fft(number)[:8])
+    #print(part2(number, 10))
+    print()
