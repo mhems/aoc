7df598e16bb07f403cd2a5cdf757d9477aee8c11
matthew <matthems@gmail.com>
2024-02-24 15:19:43 -0500

    day14 part 2

--- 2019/day14/a.py
+++ 2019/day14/a.py
@@ -30,19 +30,20 @@ def merge(d1: {str: int}, d2: {str: int}) -> {str: int}:
 
 # maps product name to (amount of product produced, amount of ore required)
 ore_counts : {str: (int, int)} = {}
+# maps product name to quantity of product leftover from reciple
 surpluses : {str: int} = defaultdict(int)
-def count_ore(table: {str: (int, (Ingredient))}, target: Ingredient = None, indent=0) :
-    global ore_counts
+
+def count_ore(table: {str: (int, (Ingredient))},
+              target: Ingredient = None,
+              indent: int = 0):
     if len(ore_counts) == 0:
         for product, (amt, ingredients) in table.items():
             if len(ingredients) == 1 and ingredients[0].name == 'ORE':
                 ore_counts[product] = amt, ingredients[0].qty
     if target is None:
         target = Ingredient(1, 'FUEL')
-    print(' ' * indent, 'looking to make', target.qty, 'of', target.name)
     target_qty = target.qty
     if surpluses[target.name] > 0:
-        print(' ' * indent, 'OOOH and i have', surpluses[target.name], 'already made')
         target_qty = max(0, target_qty - surpluses[target.name])
         surpluses[target.name] = max(0, surpluses[target.name] - target.qty)
         if target_qty == 0:
@@ -53,25 +54,35 @@ def count_ore(table: {str: (int, (Ingredient))}, target: Ingredient = None, inde
     qty, ingredients = table[target.name]
     need = ceil(target_qty/qty)
     leftover = qty * need - target_qty
-    print(' ' * indent, 'will have to make', qty * need, leftover, target.name, 'will be leftover')
-    print(' ' * indent, 'these ingredients', ingredients, 'make', qty, 'of', target.name, 'need', need)
     for ingredient in ingredients:
         ore_amts = count_ore(table, Ingredient(ingredient.qty * need, ingredient.name), indent + 2)
         amts = merge(amts, ore_amts)
-        print(' ' * indent, ' ', amts)
     if target.name == 'FUEL':
         total = 0
-        print('finally', amts)
         for name, amt in amts.items():
             produced, required = ore_counts[name]
             amt_needed = ceil(amt/produced)
-            #print(name, produced, required, amt_needed, total)
             total += amt_needed * required
-        print('surpluses:', surpluses)
         return total
-    print(' ' * indent, 'amts to make', qty * need, 'of', target.name, amts, 'surplus of', leftover)
     surpluses[target.name] += leftover
     return amts
 
+def make_fuel(ore_per_fuel, ore_amount = 1000000000000) -> int:
+    # this is off by one for example 3
+    num_fuel = 0
+    ore_remaining = ore_amount
+    while True:
+        n = ore_remaining // ore_per_fuel
+        if n == 0:
+            break
+        ore_needed = count_ore(table, Ingredient(n, 'FUEL'))
+        if ore_needed > ore_remaining:
+            break
+        ore_remaining -= ore_needed
+        num_fuel += n
+    return num_fuel
+
 table = make_table()
-print(count_ore(table))
+ore_per_fuel = count_ore(table)
+print(ore_per_fuel)
+print(make_fuel(ore_per_fuel))
