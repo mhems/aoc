e03b78441f43c922bc7b79166337abc1274c20b3
matthew <matthems@gmail.com>
2024-03-06 19:54:03 -0500

    day14

--- 2023/day14/a.py
+++ 2023/day14/a.py
@@ -1,36 +1,77 @@
 from sys import argv
 
+class TiltTable:
+    def __init__(self, text: str):
+        self.width = text.index('\n')
+        self.height = text.count('\n') + 1
+        self.grid = [list(row) for row in text.split('\n')]
+    def __str__(self) -> str:
+        return '\n'.join(''.join(row) for row in self.grid)
+    def load(self) -> int:
+        return sum(row.count('O') * (self.height - i) for i, row in enumerate(self.grid))
+    def row(self, i: int, reverse: bool = False) -> str:
+        gen = self.grid[i]
+        if reverse:
+            gen = reversed(gen)
+        return ''.join(gen)
+    def col(self, i: int, reverse: bool = False) -> str:
+        gen = (row[i] for row in self.grid)
+        if reverse:
+            gen = reversed(list(gen))
+        return ''.join(gen)
+    def tilt(self, dir: int = 0):
+        if dir == 0 or dir == 2:
+            new_cols = [self.tilt_line(self.col(x), dir == 0) for x in range(self.width)]
+            self.grid = [[col[i] for col in new_cols] for i in range(self.height)]
+        else:
+            self.grid = [self.tilt_line(self.row(x), dir == 3) for x in range(self.height)]
+    def tilt_line(self, line: str, left: bool) -> str:
+        def partition(text: str) -> [str]:
+            ps = []
+            p = ''
+            i = 0
+            while i < len(text):
+                if (c := text[i]) != '#':
+                    p += c
+                    i += 1
+                else:
+                    if p:
+                        ps.append(p)
+                    p = ''
+                    while i < len(text) and (c := text[i]) == '#':
+                        p += c
+                        i += 1
+                    ps.append(p)
+                    p = ''
+            if p:
+                ps.append(p)
+            return ps
+        def roll(s: str, left: bool) -> str:
+            if 'O' in s:
+                func = lambda x: (x.ljust(len(s), '.') if left else x.rjust(len(s), '.'))
+                return func('O' * s.count('O'))
+            return s
+        return ''.join(roll(e, left) for e in partition(line))
+    def cycle(self, n: int = 1_000_000_000) -> int:
+        seen = list()
+        first_north_load = None
+        for i in range(n):
+            self.tilt(0)
+            if first_north_load is None:
+                first_north_load = self.load()
+            self.tilt(3)
+            self.tilt(2)
+            self.tilt(1)
+            s = str(self)
+            if s in seen:
+                start = seen.index(s)
+                delta = i - start
+                return first_north_load, TiltTable(seen[start + ((n - 1 - start) % delta)]).load()
+            else:
+                seen.append(s)
+        return self.load()
+
 with open(argv[1]) as fp:
-    lines = fp.readlines()
-grid = [line.strip() for line in lines]
+    table = TiltTable(fp.read().strip())
 
-def gather_columns_as_rows(grid: [[str]]) -> [[str]]:
-    R = len(grid)
-    C = len(grid[0])
-    return [''.join(grid[R-1-r][c] for r in range(R)) for c in range(C)]
-
-def print_grid(grid: [[str]]):
-    print('\n'.join(grid))
-
-gp = gather_columns_as_rows(grid)
-
-def compute_load(col: str) -> int:
-    load = 0
-    rock_count = 0
-    for i, ch in enumerate(col):
-        if ch == 'O':
-            rock_count += 1
-        elif ch == '#':
-            load += sum(range(i + 1 - rock_count, i+1, 1))
-            #print('ledge at', i+1, ', rocks:', rock_count, 'load:', load)
-            rock_count = 0
-    load += sum(range(i + 1 - rock_count + 1, i+2, 1))
-    #print('rocks remain:', rock_count, 'load:', load)
-    return load
-
-for col in gp:
-    #print(col)
-    l = compute_load(col)
-    #print(l)
-answer = sum(compute_load(col) for col in gp)
-print(answer)
+print(table.cycle())
