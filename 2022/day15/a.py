1662ea00fee0d3161c2d860fcee6991424856a14
matthew <matthems@gmail.com>
2024-04-24 10:47:20 -0400

    day15 p2

--- 2022/day15/a.py
+++ 2022/day15/a.py
@@ -1,4 +1,6 @@
 from sys import argv
+from collections import defaultdict
+from tqdm import tqdm
 
 def distance(a: (int, int), b: (int, int)) -> int:
     return sum(abs(i - j) for i, j in zip(a, b))
@@ -8,29 +10,54 @@ def parse_line(line: str) -> ((int, int), (int, int), int):
     coords = [int(tokens[i].split('=')[1]) for i in (2, 3, 8, 9)]
     return tuple(coords[:2]), tuple(coords[2:])
 
-def get_points_within(p: (int, int), d: int) -> {(int, int)}:
-    points = set()
-    sx, sy = p
-    for d in range(d, -1, -1):
-        for dx, dy in zip(range(d + 1), range(d, -1, -1)):
-            points.add((sx + dx, sy + dy))
-            points.add((sx - dx, sy + dy))
-            points.add((sx + dx, sy - dy))
-            points.add((sx - dx, sy - dy))
-    return points
+def get_exclusion_ranges(sensor: (int, int), radius: int, bounds: (int, int)) -> {int: (int, int)}:
+    sx, sy = sensor
+    lower, upper = bounds
+    ranges = dict()
+    start = sx - radius + 1
+    end = sx + radius - 1
+    for dist in range(1, radius + 1):
+        r = (max(bounds[0], start), min(bounds[1], end) + 1)
+        if lower <= sy - dist <= upper:
+            ranges[sy - dist] = r
+        if lower <= sy + dist <= upper:
+            ranges[sy + dist] = r
+        start += 1
+        end -= 1
+    if lower <= sy <= upper:
+        ranges[sy] = max(lower, sx - radius), min(sx + radius, upper) + 1
+    return ranges
 
-def get_points_within_on_line(p: (int, int), d: int, y: int) -> {(int, int)}:
-    sx, sy = p
+def consolidate(s: {(int, int)}) -> (int, int):
+    ordered = sorted(s)
+    itr = iter(ordered)
+    start, end = next(itr)
+    for s, e in itr:
+        if s < start and e >= start:
+            start = s
+        if e > end and start <= s <= end:
+            end = e
+    return start, end
+
+def merge_ranges(d1: {int: {(int, int)}}, d2: {int: (int, int)}) -> {int: {(int, int)}}:
+    if not d1:
+        return {k: {v} for k, v in d2.items()}
+    cum = defaultdict(set, d1)
+    for y, range in d2.items():
+        cum[y].add(range)
+    return cum
+
+def get_points_within_on_line(sensor: (int, int), d: int, y: int) -> {(int, int)}:
+    sx, sy = sensor
     diff = abs(sy - y)
     new_diff = 2 * (d - diff) + 1
     points = set()
     start = sx - d + diff
-    print(new_diff)
     for x in range(start, start + new_diff):
         points.add((x, y))
     return points
 
-def make_grid(pairs: ((int, int), (int, int)), Y: int = 2000000) -> {(int, int): str}:
+def non_beacons_on_line(pairs: ((int, int), (int, int)), Y: int = 2000000) -> int:
     points = set()
     for sensor, beacon in pairs:
         d = distance(sensor, beacon)
@@ -43,5 +70,26 @@ def make_grid(pairs: ((int, int), (int, int)), Y: int = 2000000) -> {(int, int):
             points.remove(beacon)
     return len(points)
 
+def build_ranges(pairs: ((int, int), (int, int)), bounds: (int, int) = (0, 4000000)) -> int:
+    cum: {int: {(int, int)}} = dict()
+    lower, upper = bounds
+    for sensor, beacon in tqdm(pairs):
+        d = distance(sensor, beacon)
+        ranges = get_exclusion_ranges(sensor, d, bounds)
+        cum = merge_ranges(cum, ranges)
+        
+        if lower <= sensor[0] <= upper and lower <= sensor[1] <= upper:
+            cum[sensor[1]].add((sensor[0], sensor[0] + 1))
+        if lower <= beacon[0] <= upper and lower <= beacon[1] <= upper:
+            cum[beacon[1]].add((beacon[0], beacon[0] + 1))
+    for y in cum.keys():
+        if lower <= y <= upper:
+            start, end = consolidate(cum[y])
+            if start > bounds[0]:
+                return (start - 1) * bounds[1] + y
+            if end < bounds[1]:
+                return end * bounds[1] + y
+
 pairs = [parse_line(line.strip()) for line in open(argv[1]).readlines()]
-print(make_grid(pairs))
+print(non_beacons_on_line(pairs))
+print(build_ranges(pairs))
