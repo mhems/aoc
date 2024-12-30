ef5713bf7bddfbf89e5a9b9ffbb2a2150b87fe3e
matthew <matthems@gmail.com>
2024-12-30 16:24:14 -0500

    day11 part2

--- 2024/day11/a.py
+++ 2024/day11/a.py
@@ -1,59 +1,28 @@
 from sys import argv
-from collections import defaultdict
+from functools import cache
+from collections import Counter, defaultdict
 
-freq = defaultdict(int)
+@cache
+def blink(i: int) -> [int]:
+    if i == 0:
+        return [1]
+    str_ = str(i)
+    N = len(str_)
+    if N % 2 == 0:
+        return [int(str_[:N//2]), int(str_[N//2:])]
+    return [i * 2024]
 
-class StoneNode:
-    def __init__(self, a: int):
-        self.left = a
-        self.right = None
-        freq[self.left] += 1
-
-    def blink(self):
-        if self.right is not None:
-            self.left.blink()
-            self.right.blink()
-        elif self.left == 0:
-            self.left = 1
-        else:
-            str_ = str(self.left)
-            N = len(str_)
-            if N % 2 == 0:
-                self.left = StoneNode(int(str_[:N//2]))
-                self.right = StoneNode(int(str_[N//2:]))
-            else:
-                self.left *= 2024
-                
-    def __str__(self) -> str:
-        if self.right is not None:
-            return f'({self.left}, {self.right})'
-        return f'({self.left})'
-
-    def flatten(self) -> int:
-        flattened = []
-        if self.right is not None:
-            flattened.extend(self.left.flatten())
-            flattened.extend(self.right.flatten())
-        else:
-            flattened.append(self.left)
-        return flattened
-
-    def count(self) -> int:
-        flattened = 0
-        if self.right is not None:
-            flattened += self.left.count()
-            flattened += self.right.count()
-        else:
-            flattened += 1
-        return flattened
-
-def optimized_blink_many(stones: [int], n: int) -> int:
-    stones = [StoneNode(e) for e in stones]
-    for i in range(n):
-        for stone in stones:
-            stone.blink()
-        print('iteration', i, sum(stone.count() for stone in stones), sum(int(v > 1) for v in freq.values() if v > 1), flush=True)
-    return sum(stone.count() for stone in stones)
+def blink_many(stones: [int], n: int) -> int:
+    stones = Counter(stones)
+    for _ in range(n):
+        new_stones = defaultdict(int)
+        while stones:
+            stone, count = stones.popitem()
+            for e in blink(stone):
+                new_stones[e] += count
+        stones = new_stones
+    return sum(stones.values())
 
 stones = [int(stone) for stone in open(argv[1]).read().strip().split()]
-print(optimized_blink_many(stones, 25))
+print(blink_many(stones, 25))
+print(blink_many(stones, 75))
