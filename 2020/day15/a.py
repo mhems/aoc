629505375874b895f3bfddc83d2598be5fc421bf
matthew <matthems@gmail.com>
2024-12-30 17:58:52 -0500

    2020 day15 part2

--- 2020/day15/a.py
+++ 2020/day15/a.py
@@ -1,24 +1,20 @@
 from sys import argv
-from collections import defaultdict, deque
+from collections import defaultdict
 
 def play_game(nums: [int], n: int) -> int:
     memory = defaultdict(list)
-    spoken = deque()
     for i, num in enumerate(nums):
         memory[num].append(i)
         lastSpoken = num
-        spoken.append(lastSpoken)
     for i in range(len(nums), n):
         if len(memory[lastSpoken]) <= 1:
             lastSpoken = 0
         else:
             lastSpoken = memory[lastSpoken][-1] - memory[lastSpoken][-2]
         memory[lastSpoken].append(i)
-        spoken.append(lastSpoken)
-    #print(spoken)
     return lastSpoken
 
 nums = [list(map(int, line.strip().split(','))) for line in open(argv[1]).readlines()]
 for seq in nums:
     print(play_game(seq, 2020))
-    #print(play_game(seq, 30_000_000))
+    print(play_game(seq, 30_000_000))
