eafb7b91d61d598a58b7ba3fead57571529691c6
matthew <matthems@gmail.com>
2025-01-01 22:33:00 -0500

    2021 day 21 part 2

--- 2021/day21/a.py
+++ 2021/day21/a.py
@@ -1,20 +1,43 @@
 from sys import argv
 from itertools import cycle
+from collections import defaultdict
 
-def play(p1: int, p2: int, goal: int = 1000, num_die_sides: int = 100, board_size: int = 10) -> int:
+def deterministic_play(p1: int, p2: int) -> int:
     ps = [p1 - 1, p2 - 1]
     scores = [0, 0]
-    die = cycle(range(1, num_die_sides + 1))
+    die = cycle(range(1, 101))
     num_rolls = 0
-
     while True:
         for i in (0, 1):
             val = sum((next(die), next(die), next(die)))
             num_rolls += 3
-            ps[i] = (ps[i] + val) % board_size
+            ps[i] = (ps[i] + val) % 10
             scores[i] += ps[i] + 1
-            if any(score >= goal for score in scores):
+            if scores[i] >= 1000:
                 return min(scores) * num_rolls
 
+def take_turn(game_states: {((int, int), (int, int)): int}, winners: [int, int], player: int) -> {((int, int), (int, int)): int}:
+    chances = {3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
+    new_states = defaultdict(int)
+    for game, game_freq in game_states.items():
+        for total, freq in chances.items():
+            pos = (game[player][0] + total) % 10
+            score = game[player][1] + pos + 1
+            if score >= 21:
+                winners[player] += freq * game_freq
+            else:
+                state = ((pos, score), game[1]) if player == 0 else (game[0], (pos, score))
+                new_states[state] += freq * game_freq
+    return new_states
+
+def quantum_play(p1: int, p2: int) -> int:
+    game_states = {((p1-1, 0), (p2-1, 0)): 1}
+    winners = [0, 0]
+    while game_states:
+        game_states = take_turn(game_states, winners, 0)
+        game_states = take_turn(game_states, winners, 1)
+    return max(winners)
+
 p1, p2 = [int(line.strip().split()[-1]) for line in open(argv[1]).readlines()]
-print(play(p1, p2))
+print(deterministic_play(p1, p2))
+print(quantum_play(p1, p2))
