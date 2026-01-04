a62be6e32374f6532f20df00763ca25d76642508
matthew <matthems@gmail.com>
2026-01-04 15:35:41 -0500

    part2

--- 2020/day17/a.py
+++ 2020/day17/a.py
@@ -1,45 +1,33 @@
 from sys import argv
-import numpy as np
+from collections import defaultdict
 
-def neighbors2d(grid, pos: (int, int), include_origin: bool) -> int:
-    y, x = pos
-    return int(sum(grid[y + dy, x + dx]
-                   for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1,))
-                   if (dy, dx) != (0, 0) or include_origin))
+deltas = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1,))
 
-def neighbors3d(grid, pos) -> int:
-    z, y, x = pos
-    return neighbors2d(grid[z-1], (y, x), True) + neighbors2d(grid[z], (y, x), False) + neighbors2d(grid[z+1], (y, x), True)
+def neighborsxd(pos: tuple, dim: int, include: bool) -> list[tuple]:
+    w, z, y, x = pos
+    if dim == 2: return [(w, z, y+dy, x+dx) for dy, dx in deltas if dy != 0 or dx != 0 or include]
+    if dim == 3: return neighborsxd((w, z-1, y, x), 2, True) + neighborsxd((w, z, y, x), 2, include) + neighborsxd((w, z+1, y, x), 2, True)
+    return neighborsxd((w-1, z, y, x), 3, True) + neighborsxd((w, z, y, x), 3, False) + neighborsxd((w+1, z, y, x), 3, True)
 
-def conway(grid, n):
-    for _ in range(n):
-        todo = []
-        for z in range(10, 80):
-            for y in range(10, 80):
-                for x in range(10, 80):
-                    num_active = neighbors3d(grid, (z, y, x))
-                    if grid[z, y, x] == 1:
-                        if num_active != 2 and num_active != 3:
-                            todo.append(((z, y, x), 0))
-                    else:
-                        if num_active == 3:
-                            todo.append(((z, y, x), 1))
-        for (z, y, x), v in todo:
-            grid[z, y, x] = v
+def conway(initial, dim: int):
+    active = set(initial)
+    for _ in range(6):
+        new_active = set()
+        neighboring_active = defaultdict(int)
+        for cell in active:
+            neighbors = neighborsxd(cell, dim, dim == 4)
+            count = 0
+            for neighbor in neighbors:
+                count += int(neighbor in active)
+                neighboring_active[neighbor] += 1
+            if 2 <= count <= 3:
+                new_active.add(cell)
+        for n, amt in neighboring_active.items():
+            if amt == 3:
+                new_active.add(n)
+        active = new_active
+    return len(active)
 
-def num_on(grid) -> int:
-    count = 0
-    for z in range(10, 80):
-        for y in range(10, 80):
-            for x in range(10, 80):
-                if grid[z, y, x]:
-                    count += 1
-    return int(count)
-
-grid = np.zeros((100, 100, 100), dtype=np.uint8)
-for y, row in enumerate(open(argv[1]).readlines()):
-    for x, cell in enumerate(row.strip()):
-        if cell == '#':
-            grid[50, y + 50, x + 50] = 1
-conway(grid, 6)
-print(num_on(grid))
+active = {(0, 0, y, x) for y, row in enumerate(open(argv[1]).readlines()) for x, cell in enumerate(row.strip()) if cell == '#'}
+print(conway(active, 3))
+print(conway(active, 4))
