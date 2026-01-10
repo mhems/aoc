79fb013c94a7011f6377e5ed15083ff759212917
matthew <matthems@gmail.com>
2026-01-10 14:42:39 -0500

    redo without matrix math and finish part2

--- 2019/day16/a.py
+++ 2019/day16/a.py
@@ -1,51 +1,74 @@
-from sys import argv
+import sys
 import numpy as np
-from math import ceil
+from tqdm import tqdm
 
-def make_pattern(i: int, n: int) -> [int]:
-    pattern = [0] * i
-    count = i
-    if count < n + 1:
-        pattern += [1] * i
-        count += i
-    if count < n + 1:
-        pattern += [0] * i
-        count += i
-    if count < n + 1:
-        pattern += [-1] * i
-        count += i
-    if count < n + 1:
-        rep = ceil(n/len(pattern)) + 1
-        pattern *= rep
-    pattern.pop(0)
-    if len(pattern) > n:
-        pattern = pattern[:n]
-    return pattern
+mod = lambda n: abs(n) % 10
 
-def make_patterns(n: int):
-    return np.array([make_pattern(i + 1, n) for i in range(n)])
+def phase(digits: np.array):
+    n = len(digits)
+    result = np.zeros(n, dtype=np.int32)
+    mid = n//2
+    result[0] = digits[0]
+    running = digits[0]
+    for i in range(1, mid):
+        running -= digits[i-1]
+        running += digits[i+i-1] + digits[i+i]
+        result[i] = running
+    running -= digits[mid-1]
+    running += digits[-1]
+    result[mid] = running % 10
+    offset = mid
+    for i in range(mid + 1, n):
+        running -= digits[offset]
+        offset += 1
+        result[i] = running % 10
+    start = 2
+    row = 0
+    while start < n:
+        factor = -1
+        i = start
+        stride = row + 1
+        while i < n:
+            result[row] += factor * sum(digits[i:i+stride])
+            i += 2 * stride
+            factor *= -1
+        result[row] = mod(result[row])
+        start += 3
+        row += 1
+    assert row < mid
+    for i in range(row, mid):
+        result[i] = mod(result[i])
+    return result
 
-matmod = np.vectorize(lambda n: abs(n) % 10)
+def phases(seed):
+    signal = seed
+    for _ in range(100):
+        signal = phase(signal)
+    return signal[:8]
 
-def fft(number: [int], pattern) -> [int]:
-    product = np.matmul(pattern, number)
-    return matmod(product)
+def phase_part2(digits: np.array):
+    n = len(digits)
+    result = np.zeros(n, dtype=np.int32)
+    running = sum(digits)
+    result[0] = running % 10
+    for i in range(1, n):
+        running -= digits[i-1]
+        result[i] = running % 10
+    return result
 
-def phased_fft(number, phases: int = 100) -> str:
-    pattern = make_patterns(len(number))
-    number = [int(digit) for digit in number]
-    number = np.array(number).reshape((len(number), 1))
-    for _ in range(phases):
-        number = fft(number, pattern)
-    return ''.join(map(str, number.flat))
+def phases2(seed, repeat=10_000) -> str:
+    index = int(''.join(map(str, seed[:7])))
+    s = len(seed)
+    start = index//s * s
+    amt = s * repeat - start
+    signal = np.tile(seed, amt//s)[:amt]
+    for _ in tqdm(range(100)):
+        signal = phase_part2(signal)
+    offset = index - start
+    return signal[offset:offset+8]
 
-def part2(number: str, repeat=10_000) -> str:
-    index = int(number[:7])
-    result = phased_fft(number * repeat)
-    return ''.join(str(result[i][0]) for i in range(index, index+8))
-
-numbers = [line.strip() for line in open(argv[1]).readlines()]
-for number in numbers:
-    print(phased_fft(number)[:8])
-    #print(part2(number, 10))
-    print()
+numbers = [np.array(list(map(int, line.strip())), dtype=np.int32)
+           for line in open(sys.argv[1]).readlines()]
+for num in numbers:
+    print(''.join(map(str, phases(num))))
+    print(''.join(map(str, phases2(num))))
