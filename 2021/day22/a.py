99a76cc02cf2929ff2934a19c96cd32236960157
matthew <matthems@gmail.com>
2026-01-18 22:53:02 -0500

    use inclusion/exclusion instead of brute force

--- 2021/day22/a.py
+++ 2021/day22/a.py
@@ -1,33 +1,55 @@
 from sys import argv
-from collections import namedtuple as nt
-import numpy as np
+from math import prod
 
-Range = nt('Range', ['start', 'end'])
-Step = nt('Step', ['on', 'x', 'y', 'z'])
+class Cuboid:
+    def __init__(self, x, y, z, factor: int):
+        self.min = x[0], y[0], z[0]
+        self.max = x[1], y[1], z[1]
+        self.factor = factor
+    @property
+    def volume(self) -> int:
+        return self.factor * prod(b - a + 1 for a, b in zip(self.min, self.max))
+    def intersect(self, other):
+        mins, maxs = [0] * 3, [0] * 3
+        intersect = True
+        for i in range(3):
+            mins[i] = max(self.min[i], other.min[i])
+            maxs[i] = min(self.max[i], other.max[i])
+            if mins[i] > maxs[i]:
+                intersect = False
+        if intersect:
+            return Cuboid(*zip(mins, maxs), -self.factor)
+        return None
 
-def parse_range(text: str) -> Range:
-    first_dot = text.index('.')
-    start = int(text[:first_dot])
-    end = int(text[first_dot + 2:])
-    return Range(start, end)
+class World:
+    fifty = Cuboid((-50, 50), (-50, 50), (-50, 50), 1)
+    def __init__(self):
+        self.cuboids = []
+    def num_on(self) -> tuple[int, int]:
+        v1, v2 = 0, 0
+        for cuboid in self.cuboids:
+            intersection = cuboid.intersect(World.fifty)
+            if intersection is not None:
+                v1 += cuboid.factor * abs(intersection.volume)
+            v2 += cuboid.volume
+        return v1, v2
+    def add_region(self, new):
+        new_cuboids = []
+        for cuboid in self.cuboids:
+            intersection = cuboid.intersect(new)
+            if intersection is not None:
+                new_cuboids.append(intersection)
+        if len(new_cuboids) > 0:
+            self.cuboids.extend(new_cuboids)
+        if new.factor == 1:
+            self.cuboids.append(new)
 
-def parse_step(text: str) -> Step:
-    return Step(text.startswith('on'),
-                *[parse_range(token[token.index('=') + 1:])
-                 for token in text[text.index('x'):].split(',')])
+def parse_step(text: str) -> Cuboid:
+    return Cuboid(*[tuple(map(int, token[token.index('=') + 1:].split('..')))
+                    for token in text[text.index('x'):].split(',')],
+                  1 if text.startswith('on') else -1)
 
-def reboot(steps: [Step], n: int = 50) -> np.ndarray:
-    grid = np.ndarray((2*n + 1, 2*n + 1, 2*n + 1), dtype=np.dtype(bool))
-    grid.fill(False)
-    for step in steps:
-        sub = grid[(step.x.start + n): (step.x.end + n + 1),
-                   (step.y.start + n): (step.y.end + n + 1),
-                   (step.z.start + n): (step.z.end + n + 1)]
-        sub.fill(step.on)
-    return grid
-
-def num_on(grid: np.ndarray) -> int:
-    return grid.sum()
-
-steps = [parse_step(line.strip()) for line in open(argv[1]).readlines()]
-print(num_on(reboot(steps)))
+world = World()
+for cuboid in [parse_step(line.strip()) for line in open(argv[1]).readlines()]:
+    world.add_region(cuboid)
+print('\n'.join(map(str, world.num_on())))
