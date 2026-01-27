22a463d47407d95b62c1ec2f3b5dc8fe1922a428
matthew <matthems@gmail.com>
2026-01-26 21:49:12 -0500

    2022 day16 part2

--- 2022/day16/a.py
+++ 2022/day16/a.py
@@ -1,59 +1,76 @@
 from sys import argv
 from collections import deque
-from itertools import permutations
+from itertools import combinations
+from tqdm import tqdm
 
-def parse() -> ({str: (int, {str})}):
+def parse() -> ({str: {str}}, {str: int}):
     adj = dict()
+    flows = dict()
     for line in open(argv[1]).readlines():
         tokens = line.strip().replace('=', ' ').replace(';', '').split()
-        adj[tokens[1]] = int(tokens[5]), set(''.join(tokens[10:]).split(','))
-    return adj
+        flow = int(tokens[5])
+        if flow > 0:
+            flows[tokens[1]] = flow
+        adj[tokens[1]] = set(''.join(tokens[10:]).split(','))
+    return adj, flows
 
-def bfs(graph: {str: (int, {str})}, start: str, end: str) -> int:
-    q = deque()
-    visited = set()
-    q.append((start, 0))
-    visited.add(start)
+def bfs(graph: {str: {str}}, start: str, end: str) -> int:
+    q = deque([(start, 0)])
+    visited = {start}
     while q:
         cur, length = q.popleft()
         if cur == end:
             return length
-        for n in graph[cur][1]:
+        for n in graph[cur]:
             if n not in visited:
                 visited.add(n)
                 q.append((n, length + 1))
    
-def all_pairs_distances(graph: {str: (int, {str})}, valves: {str}) -> {(str, str): int}:
+def all_pairs_distances(graph: {str: {str}}, valves: {str}) -> {(str, str): int}:
     distances = {}
-    for u, v in permutations(valves, 2):
+    for u, v in combinations(valves, 2):
         distances[(u, v)] = bfs(graph, u, v)
         distances[(v, u)] = distances[(u, v)]
     return distances
 
-def psearch(graph: {str: (int, int)}, valves: {str}, distances: {(str, str): int}, time: int) -> int:
-    q = deque()
-    def enqueue(cur: str, next: str, visited: [int], initial_pressure: int, remaining_valves: {int}, remaining_time: int):
-        nonlocal q, distances, graph
-        left = distances[(cur, next)] + 1
-        minute = remaining_time - left
-        q.append((next, list(visited) + [next], set(remaining_valves) - {next}, initial_pressure + graph[next][0] * minute, minute))
-    biggest = 0
-    for valve in valves:
-        enqueue('AA', valve, [], 0, valves, time)
+graph, valves = parse()
+ids = {key: 1 << i for i, key in enumerate(valves.keys())}
+all_ids = (1 << len(valves.keys())) - 1
+distances = all_pairs_distances(graph, valves.keys() | {'AA'})
+
+def part1(mask: int = all_ids, time: int = 30) -> int:
+    q = deque([('AA', 0, 0, time)])
+    visited = dict()
+    best = 0
+    subset = {k: v for k, v in valves.items() if ids[k] & mask}
     while q:
-        cur, visited, todo, pressure, togo = q.popleft()
-        if len(todo) == 0 or togo <= 0:
-            if pressure > biggest:
-                biggest = pressure
+        cur, total, opened, time_remaining = q.popleft()
+        best_so_far = visited.get(time_remaining)
+        if best_so_far is not None:
+            if total < best_so_far:
+                continue
+        visited[time_remaining] = total
+        if time_remaining <= 0 or opened == mask:
+            if total > best:
+                best = total
             continue
-        for v in todo:
-            enqueue(cur, v, visited, pressure, todo, togo)
-    return biggest
+        for v in subset.keys():
+            if v != cur and ids[v] & opened == 0:
+                distance = distances[(cur, v)]
+                if distance <= time_remaining:
+                    q.append((v, total + subset[v] * (time_remaining - 1 - distance), opened | ids[v], time_remaining - 1 - distance))
+    return best
 
-def find_max_pressure(graph: {str: (int, {str})}, time: int = 30) -> int:
-    valves = {v for v, (rate, _) in graph.items() if rate > 0}
-    distances = all_pairs_distances(graph, valves | {'AA'})
-    return psearch(graph, valves, distances, time)
+def part2() -> int:
+    answers = [(i, part1(i, 26)) for i in tqdm(range(all_ids + 1))]
+    best = 0
+    for k, v in tqdm(answers[:len(answers)//2 + 1]):
+        for i, j in answers:
+            if k & i == 0:
+                total = v + j
+                if total > best:
+                   best = total
+    return best
 
-graph = parse()
-print(find_max_pressure(graph))
+print(part1())
+print(part2())
