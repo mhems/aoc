b239f12cab336b3a609f00cbb9522efaad4057d2
matthew <matthems@gmail.com>
2026-02-09 21:50:38 -0500

    2024 day 20 part2 (and reframe of part1 brute force

--- 2024/day20/a.py
+++ 2024/day20/a.py
@@ -1,6 +1,5 @@
 from sys import argv
 from collections import deque
-from functools import cache
 from tqdm import tqdm
 
 def build_gridmap(grid: [str]) -> ((int, int), (int, int), {(int, int)}, {(int, int)}):
@@ -22,27 +21,46 @@ def build_gridmap(grid: [str]) -> ((int, int), (int, int), {(int, int)}, {(int, 
                 path.add((y, x))
     return start, end, walls, path
 
-@cache
 def neighbors(pos: (int, int)) -> {(int, int)}:
     return {(pos[0] + dy, pos[1] + dx) for dy, dx in [(-1, 0), (0, -1), (0, 1), (1, 0)]}
 
-def bfs(open: {(int, int)}, start: (int, int), end: (int, int)) -> int:
+def bfs(open, start: (int, int), end: (int, int)) -> int:
     q = deque()
-    visited = set()
-    q.append((start, 0))
+    visited = set([start])
+    q.append(start)
+    track = []
     while q:
-        pos, path = q.popleft()
+        pos = q.popleft()
+        track.append(pos)
         if pos == end:
-            return path
-        for n in neighbors(pos):
+            return track
+        for n in neighbors(pos):    
             if n in open and n not in visited:
                 visited.add(n)
-                q.append((n, path + 1))
+                q.append(n)
 
-def find_cheats(walls: {(int, int)}, open: {(int, int)}, start: (int, int), end: (int, int)) -> int:
-    standard = bfs(open, start, end)
-    return sum(int(standard - bfs(set(open) | {wall}, start, end) >= 100) for wall in tqdm(walls))
+def find_cheats(track, n) -> int:
+    N = len(track)
+    exit = N - n
+    part1, part2 = 0, 0
+    for i, u in tqdm(enumerate(track), total=exit):
+        if i > exit:
+            break
+        j = i+n
+        track_dist = 0
+        while j < N:
+            v = track[j]
+            manhattan_dist = abs(v[0] - u[0]) + abs(v[1] - u[1])
+            if track_dist >= manhattan_dist:
+                if manhattan_dist <= 20:
+                    part2 += 1
+                    if manhattan_dist <= 2:
+                        part1 += 1
+            j += 1
+            track_dist += 1
+    return part1, part2
 
 grid = [line.strip() for line in open(argv[1]).readlines()]
-start, end, walls, path = build_gridmap(grid)
-print(find_cheats(walls, path, start, end))
+start, end, _, open = build_gridmap(grid)
+track = bfs(open, start, end)
+print(find_cheats(track, 50 if argv[1][0] == 'e' else 100))
